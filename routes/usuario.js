87cefe8bec568ff6bc21a16f88aa5c5c6fa3df37
juanma27719 <juanma27719@gmail.com>
2018-04-10 11:01:27 -0500

    Usuario token listo

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
+var mdAutenticacion = require('../middlewares/autenticacion');
 var app = express();
 
 var Usuario = require('../models/usuario');
 
-//Rutas
+// =========================================
+// Obtener todos los usuarios
+// =========================================
+
 app.get('/', (request, response, next) => {
 
     Usuario.find({}, 'nombre email img role')
@@ -29,5 +35,120 @@ app.get('/', (request, response, next) => {
 
 });
 
+// =========================================
+// Actualizar nuevo usuario
+// =========================================
+
+app.put('/:id', mdAutenticacion.verificaToken, (request, response) => {
+    var id = request.params.id;
+    var body = request.body;
+
+    Usuario.findById(id, (err, usuario) => {
+        if (err) {
+            return response.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario!',
+                errors: err
+            });
+        }
+
+        if (!usuario) {
+            return response.status(400).json({
+                ok: false,
+                mensaje: 'Usuario no existe!',
+                errors: { message: 'Usuario no encontrado!' }
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+        usuario.save((err, usuarioUpdate) => {
+            if (err) {
+                return response.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar usuario!',
+                    errors: err
+                });
+            }
+
+            usuarioUpdate.password = '**********';
+
+            response.status(200).json({
+                ok: true,
+                usuario: usuarioUpdate
+            });
+        });
+
+    });
+
+});
+
+// =========================================
+// Crear nuevo usuario
+// =========================================
+
+app.post('/', mdAutenticacion.verificaToken, (request, response) => {
+    var body = request.body;
+
+    var usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        img: body.img,
+        role: body.role
+    });
+
+    usuario.save((err, newUsuario) => {
+
+        if (err) {
+            return response.status(400).json({
+                ok: false,
+                mensaje: 'Error al crear usuario!',
+                errors: err
+            });
+        }
+
+        response.status(201).json({
+            ok: true,
+            usuario: newUsuario,
+            usuarioToken: request.usuario
+        });
+    });
+});
+
+// =========================================
+// Eliminar usuario
+// =========================================
+
+app.delete('/:id', mdAutenticacion.verificaToken, (request, response) => {
+    var id = request.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioDelete) => {
+
+        if (err) {
+            return response.status(500).json({
+                ok: false,
+                mensaje: 'Error al borrar usuario!',
+                errors: err
+            });
+        }
+
+        if (!usuarioDelete) {
+            return response.status(400).json({
+                ok: false,
+                mensaje: 'Usuario no existe!',
+                errors: { message: 'Usuario no existe!' }
+            });
+        }
+
+        response.status(200).json({
+            ok: true,
+            usuario: usuarioDelete
+        });
+
+    });
+});
 
 module.exports = app;
