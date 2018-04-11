79834c19f2fb564ffeff790b26f4990665a9a4c6
juanma27719 <juanma27719@gmail.com>
2018-04-11 10:17:19 -0500

    Seccion 12 terminada BACKEND

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
+//SEED
 module.exports.SEED = 'este-es-un-seed-para-ject-app';
+
+//GOOGLE
+module.exports.CLIENT_ID = '628241211142-dbe8ej3e13qjnss8rgnlqc40qc9q91l2.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -5,10 +5,121 @@ var SEED = require('../config/config').SEED;
 
 var app = express();
 
+//Google
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+//Modelo
 var Usuario = require('../models/usuario');
 
+
 // =========================================
-// Metodo de Login
+// Metodo de Login Google
+// =========================================
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+app.post('/google', async(request, response) => {
+
+    var token = request.body.token;
+    var googleUser = await verify(token).catch(err => {
+        return response.status(403).json({
+            ok: false,
+            mensaje: 'Token no valido',
+        });
+    });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioBD) => {
+        if (err) {
+            return response.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario!',
+                errors: err
+            });
+        }
+
+        if (usuarioBD) {
+            if (usuarioBD.google === false) {
+                return response.status(400).json({
+                    ok: false,
+                    mensaje: 'Debes usar la autenticación normal!',
+                });
+            } else {
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+
+                response.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+            }
+        } else {
+            //USUARIO NO EXISTE - SE CREA USUARIO
+            var usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = '**********';
+
+            usuario.save((err, usuarioDB) => {
+
+                if (err) {
+                    return response.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al crear usuario!',
+                        errors: err
+                    });
+                }
+
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+
+                response.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+
+            })
+
+
+        }
+    });
+
+    /*response.status(200).json({
+        ok: true,
+        mensaje: 'OK!',
+        googleUser: googleUser
+    });*/
+
+});
+
+
+
+// =========================================
+// Metodo de Login Normal
 // =========================================
 
 app.post('/', (request, response) => {
@@ -43,6 +154,7 @@ app.post('/', (request, response) => {
 
         //CREAR TOKEN
         usuarioDB.password = '**********';
+
         var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
 
         response.status(200).json({
